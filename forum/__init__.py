4d97b80b7d771539d310d8cba17971b21ac22d23
jason zhu <zjxuan1996@outlook.com>
2018-11-25 14:55:53 -0600

    add database settings

--- forum/__init__.py
+++ forum/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from .database import init_app, rebuild_db
 
 config = {
     'SESSION_TYPE': 'memcached',
@@ -7,13 +8,13 @@ config = {
         'wustl_pass@ec2-52-14-93-16.us-east-2.compute.amazonaws.com:3306/orm_forum',
     'SQLALCHEMY_TRACK_MODIFICATIONS': True,
     'SESSION_COOKIE_HTTPONLY': True,
+    'SECRET_KEY': 'super secret key',
 }
 
 
 def create_app(test_config=None):
     _app = Flask(__name__)
 
-    _app.secret_key = 'super secret key'
     _app.config.from_mapping(config)
     if test_config is not None:
         _app.config.from_object(test_config)
@@ -21,6 +22,9 @@ def create_app(test_config=None):
     from forum.home import bp_home
 
     _app.register_blueprint(bp_home)
+
+    with _app.app_context():
+        init_app()
 
     return _app
 
